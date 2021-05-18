8c8e1ea9c86b5f0c344ae15809b9382349a43535
Priyanshu Gaurav <gauravpriyanshu3@gmail.com>
2021-05-18 20:54:08 +0530

    Adding Joke controller. closes #4

---
+++ src/main/java/spring/framework/spring5jokesapp/controller/JokeController.java
@@ -0,0 +1,23 @@
+package spring.framework.spring5jokesapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import spring.framework.spring5jokesapp.services.JokeService;
+
+@Controller
+public class JokeController {
+    private final JokeService jokeService;
+
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/",""})
+    public String showJoke(Model model){
+        model.addAttribute("index",jokeService.getJoke());
+        return "index";
+    }
+
+
+}
